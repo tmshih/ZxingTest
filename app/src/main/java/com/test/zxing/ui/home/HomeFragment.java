0c1a369ea4eb99628578573afa6a832b4a4308ea
tm.shih <tm.shih@advantech.com.tw>
2020-03-12 15:17:08 +0800

    Add example to display QR-code in HomeFragment.

--- app/src/main/java/com/test/zxing/ui/home/HomeFragment.java
+++ app/src/main/java/com/test/zxing/ui/home/HomeFragment.java
@@ -1,9 +1,11 @@
 package com.test.zxing.ui.home;
 
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
@@ -12,10 +14,18 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.WriterException;
+import com.google.zxing.common.BitMatrix;
 import com.test.zxing.R;
 
 public class HomeFragment extends Fragment {
 
+    private final static int WHITE = 0xFFFFFFFF;
+    private final static int BLACK = 0xFF000000;
+    private final static int WIDTH = 400;
+    private final static int HEIGHT = 400;
     private HomeViewModel homeViewModel;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
@@ -30,6 +40,43 @@ public class HomeFragment extends Fragment {
                 textView.setText(s);
             }
         });
+        final ImageView imageView = root.findViewById(R.id.imageView);
+        homeViewModel.getQRText().observe(this, new Observer<String>() {
+            @Override
+            public void onChanged(@Nullable String s) {
+                try {
+                    Bitmap bitmap = encodeAsBitmap(s);
+                    imageView.setImageBitmap(bitmap);
+                } catch (WriterException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
         return root;
+    }
+
+    private Bitmap encodeAsBitmap(String str) throws WriterException {
+        BitMatrix result;
+        try {
+            result = new MultiFormatWriter().encode(str,
+                    BarcodeFormat.QR_CODE, WIDTH, HEIGHT, null);
+        } catch (IllegalArgumentException iae) {
+            // Unsupported format
+            return null;
+        }
+
+        int w = result.getWidth();
+        int h = result.getHeight();
+        int[] pixels = new int[w * h];
+        for (int y = 0; y < h; y++) {
+            int offset = y * w;
+            for (int x = 0; x < w; x++) {
+                pixels[offset + x] = result.get(x, y) ? BLACK : WHITE;
+            }
+        }
+
+        Bitmap bitmap = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888);
+        bitmap.setPixels(pixels, 0, w, 0, 0, w, h);
+        return bitmap;
     }
 }
--- app/src/main/java/com/test/zxing/ui/home/HomeViewModel.java
+++ app/src/main/java/com/test/zxing/ui/home/HomeViewModel.java
@@ -7,13 +7,20 @@ import androidx.lifecycle.ViewModel;
 public class HomeViewModel extends ViewModel {
 
     private MutableLiveData<String> mText;
+    private MutableLiveData<String> mQRText;
 
     public HomeViewModel() {
         mText = new MutableLiveData<>();
         mText.setValue("This is home fragment");
+        mQRText = new MutableLiveData<>();
+        mQRText.setValue("https://www.youtube.com.tw");
     }
 
     public LiveData<String> getText() {
         return mText;
+    }
+
+    public LiveData<String> getQRText() {
+        return mQRText;
     }
 }
